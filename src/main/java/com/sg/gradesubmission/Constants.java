1f3bd846fbb08321515b333fd9e6e65bf1886faa
Goel <SG8788@zebra.com>
2024-01-30 13:15:45 +0530

    complete app

---
+++ src/main/java/com/sg/gradesubmission/Constants.java
@@ -0,0 +1,5 @@
+package com.sg.gradesubmission;
+
+public class Constants {
+    public static final int NOT_FOUND = -1000;
+}
---
+++ src/main/java/com/sg/gradesubmission/GradeSubmissionApplication.java
@@ -0,0 +1,14 @@
+package com.sg.gradesubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GradeSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeSubmissionApplication.class, args);
+	}
+
+}
